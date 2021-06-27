c4c7991abf256ab219c148720e460de01b835719
ales-san <lsvwu12@gmail.com>
2021-06-27 15:45:47 +0300

    Add driver database

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            Vehicle v = new Tram("GTI", "K842YE", "John Doe");
+            Vehicle v = new Tram("GTI", "K842YE", 123);
             System.out.println(v.getVehicleType() + " " + v.getName() + " "
-                    + v.getNumber() + " " + v.getOwner() + " "
+                    + v.getNumber() + " " + v.getOwnerId() + " "
                     + v.getRequiredAge() + " " + v.getRequiredCategory());
         } catch (VehicleException e) {
             System.out.println();
